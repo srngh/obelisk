5285c371cbd2b15db9c6c99f3738ca803980f06e
srngh <soeren@imhof.tech>
2025-12-05 17:02:26 +0100

    new item dialog

--- src/widgets/ob_new_item_dialog.py
+++ src/widgets/ob_new_item_dialog.py
@@ -18,30 +18,66 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from gi.repository import Adw
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, GLib
+
+import netaddr
 
 @Gtk.Template(resource_path='/io/github/srngh/obelisk/gtk/ob_new_item_dialog.ui')
 class ObNewItemDialog(Adw.PreferencesDialog):
     __gtype_name__ = 'ObNewItemDialog'
+
+    # Template Elements
+    hostname_input = Gtk.Template.Child()
+    connection_name_input = Gtk.Template.Child()
+    auth_method = Gtk.Template.Child()
+    jumphost_input = Gtk.Template.Child()
+    proxy_input = Gtk.Template.Child()
+    connection_name_input = Gtk.Template.Child()
+    port_input = Gtk.Template.Child()
+
+    cancel_button = Gtk.Template.Child()
+    confirm_button = Gtk.Template.Child()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # Template Elements
-        address_input = Gtk.Template.Child()
-        connection_name_input = Gtk.Template.Child()
-        auth_input = Gtk.Template.Child()
-        jumphost_input = Gtk.Template.Child()
-        proxy_input = Gtk.Template.Child()
-        connection_name_input = Gtk.Template.Child()
+        self.port_input.set_value(22)
 
-        cancel_button = Gtk.Template.Child()
-        confirm_button = Gtk.Template.Child()
+        self.confirm_button.connect("clicked", self.on_confirm)
+        self.cancel_button.connect("clicked", self.on_cancel)
 
-        def on_confirm(self):
-            # Do a bunch of user input validation first
+    def on_confirm(self, user_data):
+        #self.set_can_close(False) # Lock the Dialog before the minimum input is correct
+
+        """
+        Validate
+        - is hostname_input a valid IPv4 or IPv6 address
+        - is username set, else use current users name
+        - auth is kbd_interactive by default
+        - jump host can be empty (ignored for now)
+        - proxy can be empty (ignored for now)
+        - if connection title is empty, use ip address as title
+        - port is 22 by default
+        """
+        try:
+            ip = netaddr.IPAddress(self.hostname_input.get_text())
+            port = self.port_input.get_value()
+            title = self.connection_name_input.get_text() or ip
+            print(f"User Input {ip} is an IPv{ip.version} Address")
+            print(f"Title: {title}\
+            IP: {ip}\
+            Port: {int(port)}")
+            self.set_can_close(True)
+        except netaddr.AddrFormatError as e:
+            print(e)
+
+
+    def on_cancel(self, user_data):
+        self.close()
 
 
 
 
-        #print(f"connection_name_input is activatable: {self.connection_name_input.activatable()}")
-        print(f"{self.__dict__}")
+
+    #print(f"connection_name_input is activatable: {self.connection_name_input.activatable()}")
+    #print(f"{self.__dict__}")
